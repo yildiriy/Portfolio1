05fd8fbe0b4c3fdbb2e34bed3c49bface6dbca7c
yildiriy <yildiriy@gmail.com>
2022-09-08 18:03:52 +0200

    Portfolio

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/*================================= typing animation ================*/
+var typed = new Typed(".typing",{
+    strings:["","Test Engineer", "Mechatronic Engineer","Automatician", "Project Manager"],
+    typeSpeed:80,
+    Backspeed:60,
+    loop:true
+})
