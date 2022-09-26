cbfcc88a5d062717707a1c01c70b2879f4c86100
yildiriy <yildiriy@gmail.com>
2022-09-26 16:19:40 +0200

    button

--- js/script.js
+++ js/script.js
@@ -24,3 +24,30 @@ function reveal() {
   }
   
   window.addEventListener("scroll", reveal);
+
+
+// Get the modal
+var modal = document.getElementById("mySkills");
+
+// Get the button that opens the modal
+var btn = document.getElementById("iconSkills");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
