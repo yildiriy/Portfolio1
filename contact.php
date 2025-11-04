d32c62542098541ab6b48ff588e3d09d5b4e52d7
yildiriy <113118166+yildiriy@users.noreply.github.com>
2025-11-05 00:34:37 +0100

    Ensure reveal animations expose content reliably

--- contact.php
+++ contact.php
@@ -1,34 +1,52 @@
 <?php
 
+$messageOk = "Votre message a bien été envoyé ! Un retour vous sera fait dans les meilleurs délais.";
+$messageError = "L'envoi du message a échoué. Veuillez réessayer s'il vous plaît !";
 
-$message_ok = "Votre message a bien été envoyé! Un retour vous sera fait dans les meilleurs délais."
-$message_erreur = "L'envoi du message a échoué. Veuillez réessayer svp!"
-
-$nom        = $_POST['name'];
-$messagerie = $_POST['email'];
-$contenu    = $_POST['message'];
-
-$entete = "Message de : $nom\n";
-$entete = wordwrap($messagerie, 70, "\r\n");
-$corps = "Bonjour, \n\n $ contenu\n\n";
-$piedmail = "Me contacter : \n\n$nom\n$messagerie";
-
-if ($nom != "" && $messagerie != "" && $contenu !="")
-{
-    if (preg_match('#^[\w.-]+@[\w.-]+\.[a-z]{2,6}$#i', $messagerie))
-    {
-        $message = "$entente\n$corps\n$piedmail";
-        mail('yildiriy@gmail.com', 'formulaire de mon site web', $message);
-        echo $msg_ok
-    }
-    else
-    {
-        echo $msg_erreur
-    }
-}
-else
-{
-    echo $msg_erreur
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+    echo $messageError;
+    exit;
 }
 
-?>
+$name = isset($_POST['name']) ? trim($_POST['name']) : '';
+$email = isset($_POST['email']) ? trim($_POST['email']) : '';
+$subject = isset($_POST['subject']) ? trim($_POST['subject']) : '';
+$content = isset($_POST['message']) ? trim($_POST['message']) : '';
+
+if ($name === '' || $email === '' || $content === '') {
+    echo $messageError;
+    exit;
+}
+
+if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    echo $messageError;
+    exit;
+}
+
+$mailSubject = $subject !== '' ? $subject : 'Formulaire de mon site web';
+
+$bodyLines = [
+    "Message de : $name",
+    "Email : $email",
+    '',
+];
+
+if ($subject !== '') {
+    $bodyLines[] = "Sujet : $subject";
+    $bodyLines[] = '';
+}
+
+$bodyLines[] = wordwrap($content, 70, "\r\n");
+$bodyLines[] = '';
+$bodyLines[] = 'Me contacter :';
+$bodyLines[] = $name;
+$bodyLines[] = $email;
+
+$message = implode("\r\n", $bodyLines);
+$headers = sprintf("From: %s <%s>\r\nReply-To: %s\r\n", $name, $email, $email);
+
+if (mail('yildiriy@gmail.com', $mailSubject, $message, $headers)) {
+    echo $messageOk;
+} else {
+    echo $messageError;
+}
